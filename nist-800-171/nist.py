6e1dbff5149ba30b38bec9d9be8e9e80ad56e903
Travis Howerton <33614714+howieavp76@users.noreply.github.com>
2020-11-01 10:39:40 -0500

    Basic Python Script working

--- nist-800-171/nist.py
+++ nist-800-171/nist.py
@@ -6,15 +6,38 @@ from requests.auth import HTTPBasicAuth
 import json
 
 # set the catalog URL for your Atlasity instance
-url_cats = "https://atlas-dev.c2labs.com/api/catalogues"
+url_cats = "http://localhost:5000/api/catalogues"
 
+# set your bearer token (Click your name in top right and select Service Accounts, paste Bearer token from this page)
 headers = {
-   'Authorization': 'Bearer <atlasity_api_token>'
+   'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJob3dpZWF2cCIsImp0aSI6ImJkZGM0ZDQ5LTdjZjMtNDcwNS04YjNjLTkwZDg0ODAyOTdhYSIsImlhdCI6MTYwNDI0Mjc2MywiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiI4ZDhkNTQ2OC03NGY4LTQ5OWQtOTc2Yy1iY2E2NzFlMTliMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaG93aWVhdnAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbmlzdHJhdG9yIiwibmJmIjoxNjA0MjQyNzYzLCJleHAiOjE2MDQzMjkxNjMsImlzcyI6IkF0bGFzIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwLyJ9.0PNlLKFJ9UUQaQnLTmQ_w-vtTxEpq0C0mH-QPYZeqJA'
 }
+
+# setup catalog data
+cat = {
+    "title": "NIST 800-171 Rev. 2 - Protecting Controlled Unclassified Information in Nonfederal Systems and Organizations",
+    "description": "The purpose of this publication is to provide federal agencies with recommended security requirements for protecting the confidentiality of CUI: 1) when the CUI is resident in a nonfederal system and organization; 2) when the nonfederal organization is not collecting or maintaining information on behalf of a federal agency or using or operating a system on behalf of an agency, and 3) when there are no specific safeguarding requirements for protecting the confidentiality of CUI prescribed by the authorizing law, regulation, or government-wide policy for the CUI category listed in the CUI Registry.",
+    "datePublished": "2/21/2020",
+    "lastRevisionDate": "2/21/2020",
+    "url": "https://csrc.nist.gov/publications/detail/sp/800-171/rev-2/final",
+    "abstract": "The protection of Controlled Unclassified Information (CUI) resident in nonfederal systems and organizations is of paramount importance to federal agencies and can directly impact the ability of the federal government to successfully conduct its essential missions and functions. This publication provides agencies with recommended security requirements for protecting the confidentiality of CUI when the information is resident in nonfederal systems and organizations; when the nonfederal organization is not collecting or maintaining information on behalf of a federal agency or using or operating a system on behalf of an agency; and where there are no specific safeguarding requirements for protecting the confidentiality of CUI prescribed by the authorizing law, regulation, or governmentwide policy for the CUI category listed in the CUI Registry. The requirements apply to all components of nonfederal systems and organizations that process, store, and/or transmit CUI, or that provide protection for such components. The security requirements are intended for use by federal agencies in contractual vehicles or other agreements established between those agencies and nonfederal organizations.",
+    "keywords": "basic security requirement; contractor systems; Controlled Unclassified Information; CUI Registry; derived security requirement; Executive Order 13556; FIPS Publication 199; FIPS Publication 200; FISMA; NIST Special Publication 800-53; ronfederal organizations; nonfederal systems; security assessment; security control; security requirement",
+    "createdById": "8d8d5468-74f8-499d-976c-bca671e19b14",
+    "lastUpdatedById": "8d8d5468-74f8-499d-976c-bca671e19b14" }
+
+# create the catalog and printe success result
+response = requests.request("POST", url_cats, headers=headers, json=cat)
+jsonResponse = response.json()
+print("\n\nAtlasity Output\n")
+print(response.text.encode('utf8'))
+print("\nCatalog ID: " + str(jsonResponse["id"]))
+
+# get the catalog ID
+intCat = jsonResponse["id"]
 
 # load local json
 f = open('800-171.json', 'r', encoding='utf-8-sig')
-data = json.load(f,)
+data = json.load(f)
 
 # create controls array
 controls = []
@@ -22,13 +45,12 @@ controls = []
 # create a counter for records created
 intTotal = 0
 
+# your Atlasity URL
+url_sc = "http://localhost:5000/api/securitycontrols"
+
 #loop through and print the results
 for i in data["ITS-Managed"]:
 
-    #print the raw data
-    #print(i["NIST 800-171 Control Number"])
-
-    #create the object
     # create each security control
     # NOTE: Use your user ID in Atlasity which you can find under your user profile
     sc = {
@@ -42,7 +64,7 @@ for i in data["ITS-Managed"]:
     "mappings": "",
     "assessmentPlan": "",
     "weight": i["Weight"],
-    "catalogueID": 0,
+    "catalogueID": intCat,
     "createdById": "8d8d5468-74f8-499d-976c-bca671e19b14",
     "lastUpdatedById": "8d8d5468-74f8-499d-976c-bca671e19b14" }
 
@@ -52,13 +74,12 @@ for i in data["ITS-Managed"]:
     # increment the count
     intTotal += 1
 
-url_sc = "https://atlas-dev.c2labs.com/api/securitycontrols/batchcreate"
+    # create the security control
+    response = requests.request("POST", url_sc, headers=headers, json=sc)
+    jsonResponse = response.json()
+    print("\n\nSuccess - " + sc["title"])
 
-print(controls)
+# print(controls)
 
-#response = requests.request("POST", url, headers=headers, json=payload)
-#jsonResponse = response.json()
-
-#print("\n\nSuccess - Catalog created and controls uploaded.\n")
-
+# output total controls created
 print(intTotal)
