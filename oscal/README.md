19e85e7a3777d48e7220b4d8b39d2e3d9dda1a06
Travis Howerton <33614714+howieavp76@users.noreply.github.com>
2020-11-14 12:22:35 -0500

    Initial OSCAL Parsing for 800-53

--- oscal/README.md
+++ oscal/README.md
@@ -10,6 +10,24 @@ A large number of Atlasity customers, especially government, rely on publication
 - Create catalogs for Low, Moderate, High, and Privacy using the same tool
 - Fuse data from multiple sources to enrich the catalogs prior to loading (i.e. OSCAL files, 800-53 rev 5 spreadsheet, etc.)
 
+## Process
+
+- Downloaded the Excel version of 800-53 rev5 from the NIST website
+- Download the NIST OSCAL versions of 800-53 from their GitHub site
+- Cleaned the Excel file - control numbers did not match in format between the Excel version and JSON.  Did some substitution string manipulation to make the control numbers exactly match so the data could be programmatically merged
+
+## Run the Script
+
+Setup - you will need the following Atlasity items to run this script:
+
+- Get your user ID within Atlasity (the GUID on your profile) to use in the created by fields, replace existing GUIDs
+- Get your JWT Bearer token within Atlasity (from the Service Account page)
+- Your Atlasity URL to route API calls to a running Atlasity instance
+
+To load the full catalog, run the script below (NOTE: You must pass in the bearer token for your Atlasity user, must be an admin or maintainer account, and do include the 'Bearer' portion):
+
+`py importer.py eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJob3dpZWF2cCIsImp0aSI6ImJhZjI5MDgxLTYwNTEtNGQzNy05NWY5LTc3YmI1Y2M0NjE5OSIsImlhdCI6MTYwNTM2MTkzNywiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiI4ZDhkNTQ2OC03NGY4LTQ5OWQtOTc2Yy1iY2E2NzFlMTliMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2gsYWltcy9uYW1lIjoiaG93aWVhdnAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbmlzdHJhdG9yIiwibmJmIjoxNjA1MzYxOTM2LCJleHAiOjE2MDU0NDgzMzYsImlzcyI6IkF0bGFzIiwiYXVkIjoiaHR0cDovL2xvY2FtaG9zdDo1MDAwLyJ9.46Nq59adKvXajdADCAz__WZxeD-BzRYZ9dnW5QmSdGo`
+
 ## References
 
 - [NIST OSCAL Website](https://pages.nist.gov/OSCAL/) 
@@ -19,3 +37,13 @@ A large number of Atlasity customers, especially government, rely on publication
 ## Useful Tools
 
 The JSON files generate by the OSCAL team are large and need easy to visually parse by humans.  We leveraged a free online tool - [JSON Viewer](http://jsonviewer.stack.hu/) - that allows you interactively drill into the data.  This tool was a nice complement for our developers to assist with working on this large data set.  In addition, we used the [Beautify Tools](http://beautifytools.com/excel-to-json-converter.php) to do an Excel to JSON conversion to allow for easier programmatic manipulation of the data.
+
+## OSCAL Feedback
+
+Below is a running log of feedback on enhancements to OSCAL that would have made this import easier:
+
+- The 800-53 rev 5 spreadsheet from NIST and OSCAL use different numbering formats for control enhancements.  This difference requires some pre-processing.  Some feedback that would help:
+    - Update the spreadsheet to match the OSCAL naming convention, or vice-versa
+    - Ideally, add the OSCAL control GUID to the spreadsheet as a new column
+- Some resources in the NIST 800-53 rev5 catalog have no data, requires more error handling in the code
+- When fields have no data, they are removed from the OSCAL files.  Would prefer them to default to an empty string so they are always there.  Allows for more efficient looping without constantly checking if the data exists.
--- oscal/importer.py
+++ oscal/importer.py
@@ -4,40 +4,116 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import argparse
+
+# setup parser for command line arguments
+parser = argparse.ArgumentParser(description='Atlasity parser for NIST 800-53 OSCAL')
+parser.add_argument('token', metavar='path', type=str, help='Atlasity JWT token to authenticate API calls')
+
+# get the argument from the command line
+args = parser.parse_args()
+if (args.token == ''):
+    print('No JWT Bearer token provided.')
+else:
+    print(args.token)
+    token = args.token
 
 # set the catalog URL for your Atlasity instance
 url_cats = "http://localhost:5000/api/catalogues"
 
 # set your bearer token (Click your name in top right and select Service Accounts, paste Bearer token from this page)
 headers = {
-   'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJob3dpZWF2cCIsImp0aSI6ImJkZGM0ZDQ5LTdjZjMtNDcwNS04YjNjLTkwZDg0ODAyOTdhYSIsImlhdCI6MTYwNDI0Mjc2MywiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiI4ZDhkNTQ2OC03NGY4LTQ5OWQtOTc2Yy1iY2E2NzFlMTliMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaG93aWVhdnAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbmlzdHJhdG9yIiwibmJmIjoxNjA0MjQyNzYzLCJleHAiOjE2MDQzMjkxNjMsImlzcyI6IkF0bGFzIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwLyJ9.0PNlLKFJ9UUQaQnLTmQ_w-vtTxEpq0C0mH-QPYZeqJA'
+   'Authorization': 'Bearer ' + token
 }
 
 # setup catalog data
 cat = {
-    "title": "NIST 800-171 Rev. 2 - Protecting Controlled Unclassified Information in Nonfederal Systems and Organizations",
-    "description": "The purpose of this publication is to provide federal agencies with recommended security requirements for protecting the confidentiality of CUI: 1) when the CUI is resident in a nonfederal system and organization; 2) when the nonfederal organization is not collecting or maintaining information on behalf of a federal agency or using or operating a system on behalf of an agency, and 3) when there are no specific safeguarding requirements for protecting the confidentiality of CUI prescribed by the authorizing law, regulation, or government-wide policy for the CUI category listed in the CUI Registry.",
-    "datePublished": "2/21/2020",
-    "lastRevisionDate": "2/21/2020",
-    "url": "https://csrc.nist.gov/publications/detail/sp/800-171/rev-2/final",
-    "abstract": "The protection of Controlled Unclassified Information (CUI) resident in nonfederal systems and organizations is of paramount importance to federal agencies and can directly impact the ability of the federal government to successfully conduct its essential missions and functions. This publication provides agencies with recommended security requirements for protecting the confidentiality of CUI when the information is resident in nonfederal systems and organizations; when the nonfederal organization is not collecting or maintaining information on behalf of a federal agency or using or operating a system on behalf of an agency; and where there are no specific safeguarding requirements for protecting the confidentiality of CUI prescribed by the authorizing law, regulation, or governmentwide policy for the CUI category listed in the CUI Registry. The requirements apply to all components of nonfederal systems and organizations that process, store, and/or transmit CUI, or that provide protection for such components. The security requirements are intended for use by federal agencies in contractual vehicles or other agreements established between those agencies and nonfederal organizations.",
-    "keywords": "basic security requirement; contractor systems; Controlled Unclassified Information; CUI Registry; derived security requirement; Executive Order 13556; FIPS Publication 199; FIPS Publication 200; FISMA; NIST Special Publication 800-53; ronfederal organizations; nonfederal systems; security assessment; security control; security requirement",
+    "title": "NIST 800-53 Rev. 5 - Security and Privacy Controls for Information Systems and Organizations",
+    "description": "This publication provides a catalog of security and privacy controls for information systems and organizations to protect organizational operations and assets, individuals, other organizations, and the Nation from a diverse set of threats and risks, including hostile attacks, human errors, natural disasters, structural failures, foreign intelligence entities, and privacy risks. ",
+    "datePublished": "9/1/2020",
+    "lastRevisionDate": "9/1/2020",
+    "url": "https://csrc.nist.gov/publications/detail/sp/800-53/rev-5/final",
+    "abstract": "This publication provides a catalog of security and privacy controls for information systems and organizations to protect organizational operations and assets, individuals, other organizations, and the Nation from a diverse set of threats and risks, including hostile attacks, human errors, natural disasters, structural failures, foreign intelligence entities, and privacy risks. The controls are flexible and customizable and implemented as part of an organization-wide process to manage risk. The controls address diverse requirements derived from mission and business needs, laws, executive orders, directives, regulations, policies, standards, and guidelines. Finally, the consolidated control catalog addresses security and privacy from a functionality perspective (i.e., the strength of functions and mechanisms provided by the controls) and from an assurance perspective (i.e., the measure of confidence in the security or privacy capability provided by the controls). Addressing functionality and assurance helps to ensure that information technology products and the systems that rely on those products are sufficiently trustworthy.",
+    "keywords": "assurance; availability; computer security; confidentiality; control; cybersecurity; FISMA; information security; information system; integrity; personally identifiable information; Privacy Act; privacy controls; privacy functions; privacy requirements; Risk Management Framework; security controls; security functions; security requirements; system; system security.",
     "createdById": "8d8d5468-74f8-499d-976c-bca671e19b14",
     "lastUpdatedById": "8d8d5468-74f8-499d-976c-bca671e19b14" }
 
-# create the catalog and printe success result
-response = requests.request("POST", url_cats, headers=headers, json=cat)
-jsonResponse = response.json()
-print("\n\nAtlasity Output\n")
-print(response.text.encode('utf8'))
-print("\nCatalog ID: " + str(jsonResponse["id"]))
+# create the catalog and print success result
+#response = requests.request("POST", url_cats, headers=headers, json=cat)
+#jsonResponse = response.json()
+#print("\n\nAtlasity Output\n")
+#print(response.text.encode('utf8'))
+#print("\nCatalog ID: " + str(jsonResponse["id"]))
 
 # get the catalog ID
-intCat = jsonResponse["id"]
+#intCat = jsonResponse["id"]
+intCat = 1
 
-# load local json
-f = open('800-171.json', 'r', encoding='utf-8-sig')
-data = json.load(f)
+# load translated Excel file (cleaned and converted to JSON)
+excel = open('80053rev5-excelConversion.json', 'r', encoding='utf-8-sig')
+excelData = json.load(excel)
+
+# load the full catalog for the NIST 800-53 OSCAL
+oscal = open('NIST_SP-800-53_rev5-FINAL_catalog.json', 'r', encoding='utf-8-sig')
+oscalData = json.load(oscal)
+
+#parse the OSCAL JSON to get related data (used to enrich base spreadsheet)
+arrL1 = oscalData["catalog"]
+strUUID = arrL1["uuid"]
+
+#process resources for lookup
+resources = []
+arrL2 = arrL1["back-matter"]
+for i in arrL2["resources"]:
+    #make sure values exist
+    strResourceTitle = ""
+    if ("title" in i):
+        strResourceTitle = i["title"]
+    strResourceGUID = ""
+    if ("uuid" in i):
+        strResourceGUID = i["uuid"]
+    strCitation = ""
+    if ("citation" in i):
+        citation = i["citation"]
+        if ("text" in citation):
+            strCitation = citation["text"]
+    #add parsed/flattened resource to the array
+    res = {
+        "uuid": strResourceGUID,
+        "short": strResourceTitle,
+        "title": strCitation
+    }
+    resources.append(res)
+
+#process NIST families of controls
+families = []
+oscalControls = []
+#process groups of controls
+for i in arrL1["groups"]:
+    strFamily = i["title"]
+    f = {
+        "id": i["id"],
+        "title": i["title"],
+    }
+    #add parsed item to the family array
+    families.append(f)
+    #loop through controls
+    for ctrl in i["controls"]:
+        strLinks = ""
+        #process control links
+        if ("links" in ctrl):
+            for l in ctrl["links"]:
+                strLinks += l["text"]
+        #add control
+        newCTRL = {
+            "id": ctrl["id"],
+            "title": ctrl["title"],
+            "family": strFamily,
+            "links": strLinks
+        }
+        oscalControls.append(newCTRL)
+
+print(oscalControls)
 
 # create controls array
 controls = []
@@ -49,23 +125,38 @@ intTotal = 0
 url_sc = "http://localhost:5000/api/securitycontrols"
 
 #loop through and print the results
-for i in data["ITS-Managed"]:
+for i in excelData["SP 800-53 Revision 5"]:
+
+    #make sure values exist
+    if ("Related Controls" in i):
+        strRelated = i["Related Controls"]
+    else: 
+        strRelated = ""
+    if ("Control (or Control Enhancement) " in i):
+        strControl = i["Control (or Control Enhancement) "]
+    else:
+        strControl = ""
+    if ("Discussion" in i):
+        strDiscussion = i["Discussion"]
+    else:
+        strDiscussion = ""
 
     # create each security control
     # NOTE: Use your user ID in Atlasity which you can find under your user profile
     sc = {
-    "title": i["NIST 800-171 Control Number"] + " - " + i["Control Text"],
-    "description": i["Discussion"],
+    "title": i["Control Number"] + " - " + i["Control (or Control Enhancement) Name"],
+    "description": strControl + "<br/><h3>Discussion</h3>" + strDiscussion,
     "references": "",
-    "relatedControls": i["NIST 800-53 Mapped Control"],
+    "relatedControls": strRelated,
     "subControls": "",
     "enhancements": "",
-    "family": i["Control Family"],
+    "family": "",
     "mappings": "",
     "assessmentPlan": "",
-    "weight": i["Weight"],
+    "weight": 0,
     "practiceLevel": "",
     "catalogueID": intCat,
+    "UUID": strUUID,
     "createdById": "8d8d5468-74f8-499d-976c-bca671e19b14",
     "lastUpdatedById": "8d8d5468-74f8-499d-976c-bca671e19b14" }
 
@@ -75,63 +166,63 @@ for i in data["ITS-Managed"]:
     # increment the count
     intTotal += 1
 
-    # attempt to create the security control
-    try:
-        response = requests.request("POST", url_sc, headers=headers, json=sc)
-        jsonResponse = response.json()
-        print("\n\nSuccess - " + sc["title"])
-    except requests.exceptions.HTTPError as errh:
-        print ("Http Error:",errh)
-        print("\n\nError - " + sc["title"])
-    except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:",errc)
-        print("\n\nError - " + sc["title"])
-    except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
-        print("\n\nError - " + sc["title"])
-    except requests.exceptions.RequestException as err:
-        print ("OOps: Something Else",err)
-        print("\n\nError - " + sc["title"])
+    # # attempt to create the security control
+    # try:
+    #     response = requests.request("POST", url_sc, headers=headers, json=sc)
+    #     jsonResponse = response.json()
+    #     print("\n\nSuccess - " + sc["title"])
+    # except requests.exceptions.HTTPError as errh:
+    #     print ("Http Error:",errh)
+    #     print("\n\nError - " + sc["title"])
+    # except requests.exceptions.ConnectionError as errc:
+    #     print ("Error Connecting:",errc)
+    #     print("\n\nError - " + sc["title"])
+    # except requests.exceptions.Timeout as errt:
+    #     print ("Timeout Error:",errt)
+    #     print("\n\nError - " + sc["title"])
+    # except requests.exceptions.RequestException as err:
+    #     print ("OOps: Something Else",err)
+    #     print("\n\nError - " + sc["title"])
 
-# output total controls created
-print(intTotal)
+# # output total controls created
+# print(intTotal)
 
-# retrieve full list created
-url_allcats = "http://localhost:5000/api/SecurityControls/filterSecurityControlsByCatalogue/" + str(intCat)
+# # retrieve full list created
+# url_allcats = "http://localhost:5000/api/SecurityControls/filterSecurityControlsByCatalogue/" + str(intCat)
 
-headers_allcats = {
-   "Accept": "application/json",
-   'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJob3dpZWF2cCIsImp0aSI6ImJkZGM0ZDQ5LTdjZjMtNDcwNS04YjNjLTkwZDg0ODAyOTdhYSIsImlhdCI6MTYwNDI0Mjc2MywiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiI4ZDhkNTQ2OC03NGY4LTQ5OWQtOTc2Yy1iY2E2NzFlMTliMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaG93aWVhdnAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbmlzdHJhdG9yIiwibmJmIjoxNjA0MjQyNzYzLCJleHAiOjE2MDQzMjkxNjMsImlzcyI6IkF0bGFzIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwLyJ9.0PNlLKFJ9UUQaQnLTmQ_w-vtTxEpq0C0mH-QPYZeqJA'
-}
+# headers_allcats = {
+#    "Accept": "application/json",
+#    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJob3dpZWF2cCIsImp0aSI6ImJkZGM0ZDQ5LTdjZjMtNDcwNS04YjNjLTkwZDg0ODAyOTdhYSIsImlhdCI6MTYwNDI0Mjc2MywiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiI4ZDhkNTQ2OC03NGY4LTQ5OWQtOTc2Yy1iY2E2NzFlMTliMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaG93aWVhdnAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbmlzdHJhdG9yIiwibmJmIjoxNjA0MjQyNzYzLCJleHAiOjE2MDQzMjkxNjMsImlzcyI6IkF0bGFzIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwLyJ9.0PNlLKFJ9UUQaQnLTmQ_w-vtTxEpq0C0mH-QPYZeqJA'
+# }
 
-allcats = requests.request(
-   "GET",
-   url_allcats,
-   headers=headers_allcats,
-)
-catsArray = allcats.json()
+# allcats = requests.request(
+#    "GET",
+#    url_allcats,
+#    headers=headers_allcats,
+# )
+# catsArray = allcats.json()
 
-# loop through and compare arrays, see if anything is missing (validation check)
-intMatch = 0
-for y in controls:
-    bMatch = False
-    # make sure each control in the raw JSON was returned from Atlasity
-    for z in catsArray:
-        if z["title"] == y["title"] :
-            bMatch = True
-            break
-    #make sure they match or throw error in console if missing
-    if bMatch == True :
-        intMatch += 1
-    else:
-        # ones that didn't upload
-        print("ERROR: " + y["title"] + " is missing.")
-        # see if it was length related
-        print(len(y["title"] ))
+# # loop through and compare arrays, see if anything is missing (validation check)
+# intMatch = 0
+# for y in controls:
+#     bMatch = False
+#     # make sure each control in the raw JSON was returned from Atlasity
+#     for z in catsArray:
+#         if z["title"] == y["title"] :
+#             bMatch = True
+#             break
+#     #make sure they match or throw error in console if missing
+#     if bMatch == True :
+#         intMatch += 1
+#     else:
+#         # ones that didn't upload
+#         print("ERROR: " + y["title"] + " is missing.")
+#         # see if it was length related
+#         print(len(y["title"] ))
 
-# validate all were loaded
-if intMatch == intTotal :
-    print("SUCCESS: Verified all controls were successfully created.")
+# # validate all were loaded
+# if intMatch == intTotal :
+#     print("SUCCESS: Verified all controls were successfully created.")
 
 
 
